4ae8f0e7a4fec53651711107c56153224f9772aa
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2024-04-24 09:08:15 +1000

    video client finished

--- client.py
+++ client.py
@@ -1,19 +1,33 @@
 import socket
+import cv2
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ("localhost", 65432)
-client_socket.connect(server_address)
-print("Connected to the server.")
-try:
-    while True:
-        message = input("Enter a message to send to the server: ")
-        if message.lower() == "exit":
-            break
+# Create a UDP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address = ("localhost", 1234)
 
-        client_socket.sendall(message.encode("utf-8"))
+# Capture video from the camera
+cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
-        data = client_socket.recv(1024)
-        print("Client received:", data.decode("utf-8"))
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
 
-finally:
-    client_socket.close()
+    # Show the frame
+    cv2.imshow("Camera Recording", frame)
+
+    # Base64 encode the frame
+    _, buffer = cv2.imencode(".jpg", frame)
+    base64_bytes = buffer.tobytes()
+    client_socket.sendto(base64_bytes, server_address)
+
+    # Manage quit
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+
+cam.release()
+cv2.destroyAllWindows()
