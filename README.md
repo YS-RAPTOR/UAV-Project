8356931e1513c556e95ffec1f53baacadc334ffa
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2024-04-24 08:13:08 +1000

    Simple Client/Server

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # UAV-Project
+
+## Questions to ask supervisor
+
+Drone Model
+
+# What needs to be discussed
+
+# Use of the UAV in the project
+
+- Agriculture
+- Bush Fires
+- Land mine locations
---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ("localhost", 65432)
+client_socket.connect(server_address)
+print("Connected to the server.")
+try:
+    while True:
+        message = input("Enter a message to send to the server: ")
+        if message.lower() == "exit":
+            break
+
+        client_socket.sendall(message.encode("utf-8"))
+
+        data = client_socket.recv(1024)  # Receive up to 1024 bytes
+        print("Client received:", data.decode("utf-8"))
+
+finally:
+    client_socket.close()
